add1177f33215bb0581a86f6a05efd506c59509a
Pango Chan <pango853@gmail.com>
2016-06-12 09:39:11 +0900

    Create plug-in from intro template

---
+++ src/info/kuee/boxer3/intro/ApplicationWorkbenchAdvisor.java
@@ -0,0 +1,24 @@
+package info.kuee.boxer3.intro;
+
+import org.eclipse.ui.application.IWorkbenchConfigurer;
+import org.eclipse.ui.application.IWorkbenchWindowConfigurer;
+import org.eclipse.ui.application.WorkbenchAdvisor;
+import org.eclipse.ui.application.WorkbenchWindowAdvisor;
+
+public class ApplicationWorkbenchAdvisor extends WorkbenchAdvisor {
+
+	private static final String PERSPECTIVE_ID = "info.kuee.boxer3.perspective";
+
+    public WorkbenchWindowAdvisor createWorkbenchWindowAdvisor(IWorkbenchWindowConfigurer configurer) {
+        return new ApplicationWorkbenchWindowAdvisor(configurer);
+    }
+    
+    public void initialize(IWorkbenchConfigurer configurer) {
+        super.initialize(configurer);
+        configurer.setSaveAndRestore(true);
+    }
+
+	public String getInitialWindowPerspectiveId() {
+		return PERSPECTIVE_ID;
+	}
+}
---
+++ src/info/kuee/boxer3/intro/Perspective.java
@@ -0,0 +1,10 @@
+package info.kuee.boxer3.intro;
+
+import org.eclipse.ui.IPageLayout;
+import org.eclipse.ui.IPerspectiveFactory;
+
+public class Perspective implements IPerspectiveFactory {
+
+	public void createInitialLayout(IPageLayout layout) {
+	}
+}
